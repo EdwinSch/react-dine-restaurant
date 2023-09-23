41e5aeb187c6eabff94e14e1e8e68179192dc069
front-ed <edwinschutjes@gmail.com>
2023-09-23 18:18:34 +0200

    add: data file

---
+++ src/data.js
@@ -0,0 +1,27 @@
+import salmon from "./assets/homepage/salmon-mobile@2x.jpg";
+import mignon from "./assets/homepage/beef-mobile@2x.jpg";
+import mousse from "./assets/homepage/chocolate-mobile@2x.jpg";
+
+export const menu = [
+  {
+    id: 1,
+    img: salmon,
+    title: "seared salmon fillet",
+    description:
+      "Our locally sourced salmon served with a refreshing buckwheat summer salad.",
+  },
+  {
+    id: 2,
+    img: mignon,
+    title: "rosemary filet mignon",
+    description:
+      "Our prime beef served to your taste with a delicious choice of seasonal sides.",
+  },
+  {
+    id: 3,
+    img: mousse,
+    title: "Summer Fruit Chocolate Mousse",
+    description:
+      "Creamy mousse combined with summer fruits and dark chocolate shavings.",
+  },
+];
